f23f8e6203e833ce556d04cb7b95e0f777210993
Damien BRY <damienbry@MacBook-Pro-de-Damien-2.local>
2016-03-01 14:25:50 +0100

    feature(gif): shared & api handling

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+
+var gif = require('../../my-awesome-stack-shared/src/shared/gif');
+var request = require('request');
+
+var api = "http://localhost:4242/gifs/random";
+
+window.randomizeFromShared = function() {
+    gif.getRandomGIF()
+        .then(function(url){
+            appendGIF(url);
+        });
+};
+
+window.randomizeFromAPI = function() {
+
+    request(api, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            appendGIF(body);
+        }else{
+            console.log('error='+JSON.stringify(error));
+        }
+    });
+};
+
+function appendGIF(url){
+    var DOM_img = document.createElement("img");
+    DOM_img.src = url;
+
+    document.getElementById("gifList").appendChild(DOM_img);
+}
